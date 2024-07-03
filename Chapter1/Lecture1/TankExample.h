edee92dbaf45bd9ff4c21f4fea297154eb22d6e3
Rachel Lee <engrrachel318@gmail.com>
2024-07-03 17:09:45 +0900

    Complete 1.3

--- Chapter1/Lecture1/TankExample.h
+++ Chapter1/Lecture1/TankExample.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "Game2D.h"
+#include <vector>
+#include <deque>
 
 namespace jm
 {
@@ -8,7 +10,7 @@ namespace jm
 	{
 	public:
 		vec2 center = vec2(0.0f, 0.0f);
-		//vec2 direction = vec2(1.0f, 0.0f, 0.0f);
+		//vec2 direction = vec2(1.0f, 0.0f, 0.0f); // 회전하는 기능
 
 		void draw()
 		{
@@ -51,43 +53,68 @@ namespace jm
 	public:
 		MyTank tank;
 
+		// 총알이 발사가 되었을 때에만 존재 -> 포인터
 		MyBullet *bullet = nullptr;
 		//TODO: allow multiple bullets
+		std::vector<MyBullet*> vBulletPtr;
 		//TODO: delete bullets when they go out of the screen
 
 	public:
 		TankExample()
 			: Game2D("This is my digital canvas!", 1024, 768, false, 2)
-		{}
+		{
+			//vBulletPtr.reserve(100);
+		}
 
 		~TankExample()
 		{
-			if(bullet != nullptr) delete bullet;
+			if (vBulletPtr.size()) {
+				for (int idx = 0; idx < vBulletPtr.size(); ++idx)
+					delete vBulletPtr[idx];
+			}
 		}
 
 		void update() override
 		{
 			// move tank
-			if (isKeyPressed(GLFW_KEY_LEFT))	tank.center.x -= 0.5f * getTimeStep();
+			// 왼화살표 키가 눌렸는지?
+			// 게임 개발 API를 사용하거나 GUI 프로그래밍 할 때에는 콜백 함수들을 많이 사용
+			if (isKeyPressed(GLFW_KEY_LEFT))	tank.center.x -= 0.5f * getTimeStep(); 
 			if (isKeyPressed(GLFW_KEY_RIGHT))	tank.center.x += 0.5f * getTimeStep();
 			if (isKeyPressed(GLFW_KEY_UP))		tank.center.y += 0.5f * getTimeStep();
 			if (isKeyPressed(GLFW_KEY_DOWN))	tank.center.y -= 0.5f * getTimeStep();
 
 			// shoot a cannon ball
-			if (isKeyPressedAndReleased(GLFW_KEY_SPACE))
+			// 눌렸다가 떨어졌는지? 등등 update함수가 호출되면서 매번 체크
+			if (isKeyPressedAndReleased(GLFW_KEY_SPACE)) 
 			{
 				bullet = new MyBullet;
 				bullet->center = tank.center;
 				bullet->center.x += 0.2f;
 				bullet->center.y += 0.1f;
 				bullet->velocity = vec2(2.0f, 0.0f);
+				vBulletPtr.push_back(bullet);
 			}
 
-			if (bullet != nullptr) bullet->update(getTimeStep());
+			if (vBulletPtr.size()) 
+			{
+				int cnt=0;
+				for (int idx=0; idx < vBulletPtr.size(); ++idx)
+				{
+					vBulletPtr[idx]->update(getTimeStep());
+					// **TODO: GUI 속성 사용해서 화면 가장자리를 벗어나면 없어지게끔 하기
+					if (vBulletPtr[idx]->center.x > 5) 
+						delete vBulletPtr[idx], ++cnt;
+				}
+				vBulletPtr.erase(vBulletPtr.begin(), vBulletPtr.begin() + cnt);
+			}
 
 			// rendering
 			tank.draw();
-			if (bullet != nullptr) bullet->draw();
+			if (vBulletPtr.size()) {
+				for (int idx = 0; idx < vBulletPtr.size(); ++idx)
+					vBulletPtr[idx]->draw();
+			}
 		}
 	};
 }
--- Chapter1/Lecture1/WalkingPerson.h
+++ Chapter1/Lecture1/WalkingPerson.h
@@ -11,47 +11,150 @@ namespace jm
 	- make an Ironman and allow for him to shoot repulsor beam with his right hand
 	*/
 
-	class WalkingPerson : public Game2D
+	class Person
 	{
-		float time = 0.0f;
-
-	public:
-		void update() override
+	private:
+		vec2 center = { 0.0f, 0.0f };
+		
+		void _drawHeadAndBody() const
 		{
 			// gold face
 			beginTransformation();
+			translate(center);
 			translate(0.0f, 0.12f);
 			drawFilledCircle(Colors::gold, 0.08f);
 			translate(0.05f, 0.03f);
 			drawFilledCircle(Colors::white, 0.01f); // while eye
-
 			endTransformation();
 
 			// red body
 			beginTransformation();
+			translate(center);
 			scale(1.0f, 2.0f);
 			translate(0.0f, -0.1f);
 			drawFilledBox(Colors::red, 0.13f, 0.2f);
 			endTransformation();
+		}
+	public:
+		void setCenter(const int& order)
+		{
+			center = { -1.0f + order * 0.5f, 0.0f };
+		}
 
-			// yellow arm
+		void shoot(const float& time) const
+		{
+			_drawHeadAndBody();
+
+			// yellow arm front
 			beginTransformation();
-			rotate(sin(time*5.0f) * 30.0f);					// animation!
-			scale(1.0f, 2.0f);
-			translate(0.0f, -0.1f);
-			drawFilledBox(Colors::yellow, 0.05f, 0.18f);
+			translate(center);
+			rotate(90.0f);
+			translate(0.0f, -0.2f);
+			drawFilledBox(Colors::yellow, 0.05f, 0.36f);
 			endTransformation();
 
-			// green leg
+			// green leg front
 			beginTransformation();
+			translate(center);
+			translate(0.0f, -0.6f);
+			drawFilledBox(Colors::green, 0.1f, 0.4f);
+			endTransformation();
+
+			// beam
+			beginTransformation();
+			translate(center);
+			translate(0.39f + 0.4f * 5 * time, 0.01f);
+			//scale(1.1f * 5 * time, 1.0f);
+			drawFilledBox(Colors::skyblue, 0.8f * 5 * time, 0.1f);
+			endTransformation();
+
+		}
+
+		void walk(const float& time) const
+		{
+			// yellow arm back
+			beginTransformation();
+			translate(center);
+			rotate(-sin(time * 5.0f) * 30.0f);					// animation!
+			translate(0.0f, -0.2f);
+			drawFilledBox(Colors::yellow, 0.05f, 0.36f);
+			endTransformation();
+
+			// green leg back
+			beginTransformation();
+			translate(center);
 			translate(0.0f, -0.6f);
 			translate(0.0f, 0.2f);
-			rotate(sinf(time*5.0f + 3.141592f) * 30.0f);	// animation!
+			rotate(-sinf(time * 5.0f + 3.141592f) * 30.0f);	// animation!
 			translate(0.0f, -0.2f);
 			drawFilledBox(Colors::green, 0.1f, 0.4f);
 			endTransformation();
 
-			time += this->getTimeStep();
+			_drawHeadAndBody();
+
+			// yellow arm front
+			beginTransformation();
+			translate(center);
+			rotate(sin(time * 5.0f) * 30.0f);					// animation!
+			translate(0.0f, -0.2f);
+			drawFilledBox(Colors::yellow, 0.05f, 0.36f);
+			endTransformation();
+
+			// green leg front
+			beginTransformation();
+			translate(center);
+			translate(0.0f, -0.6f);
+			translate(0.0f, 0.2f);
+			rotate(sinf(time * 5.0f + 3.141592f) * 30.0f);	// animation!
+			translate(0.0f, -0.2f);
+			drawFilledBox(Colors::green, 0.1f, 0.4f);
+			endTransformation();
+		}
+	};
+
+	class WalkingPerson : public Game2D
+	{
+		float time, timeShoot;
+		std::vector<Person*> vPerson;
+		int numPerson;
+
+	public:
+		WalkingPerson()
+			: time(0.0f), timeShoot(0.0f), numPerson(0)
+		{
+			vPerson.reserve(50);
+		}
+
+		~WalkingPerson()
+		{
+			if (vPerson.size())
+				for (int i = 0; i < vPerson.size(); ++i)
+					delete vPerson[i];
+		}
+
+		void update() override
+		{
+			if (isKeyPressedAndReleased(GLFW_KEY_TAB)) {
+				Person* prsn = new Person;
+				vPerson.push_back(prsn);
+				vPerson[numPerson]->setCenter(numPerson);
+				++numPerson;
+			}
+
+			if (isKeyPressed(GLFW_KEY_SPACE))
+			{
+				time = 0.0f;
+				for (int i = 0; i < vPerson.size(); ++i)
+					vPerson[i]->shoot(timeShoot);
+				timeShoot += this->getTimeStep();
+			}
+			else 
+			{
+				timeShoot = 0.0f;
+				for (int i = 0; i < vPerson.size(); ++i)
+					vPerson[i]->walk(time);
+				time += this->getTimeStep();
+			}
 		}
 	};
 }
